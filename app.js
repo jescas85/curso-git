91b84eaa954191b3d1440a9999b37f09fa84ed0f
beto <isc.jcastro@gmail.com>
2020-04-18 22:40:21 -0500

    prueba de gt version1

---
+++ app.js
@@ -0,0 +1,1 @@
+consele.console.log("hola mundo");
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primera página</title>
+    <script src=""></script>
+</head>
+
+<body>
+    <span>Esta es una archivo de practica. continuacion</span>
+</body>
+</html>
